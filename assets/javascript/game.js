181723846e77b90ed9474d591a3cc75752481862
Deezy <brad.driver33@gmail.com>
2018-09-11 13:18:26 -0400

    modified game.js by making crystals an object and modified index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,57 @@
 // generate random number for the target number and for the crystals.
-var lowerCrystalLimit = 1;
-var upperCrystalLimit = 12;
-var targetLowerLimit = 19;
-var targetUpperLimit = 120;
-var red_Num = 0;
-var diamond_Num = 0;
-var purple_Num = 0;
-var blue_Num = 0;
-var random_Num = 0; 
-var newGame = true;
+
+
+var targetScore = 0; 
+var currentScore = 0
+var wins = 0
+var losses = 0
+
+var crystals = {
+    diamond: {
+        name: "Diamond",
+        value: 0,
+    },
+    purple: {
+        name: "Purple",
+        value: 0
+    },
+    blue: {
+        name: "Blue",
+        value: 0,
+    },
+    red: {
+        name: "Red",
+        value: 0,
+    }
+}
 
 resetGame();
 
-// get random numbers for the crystals and the target number
-function randomTarget(targetLowerLimit, targetUpperLimit) {
-    return Math.floor(Math.random() * (targetUpperLimit - targetLowerLimit)) + targetLowerLimit;
-};
-function getRandom(lowerCrystalLimit, upperCrystalLimit) {
-    return Math.floor(Math.random() * (upperCrystalLimit - lowerCrystalLimit)) + lowerCrystalLimit;
-};
-getRandom();
-function resetGame() {
-    red_Num = getRandom(lowerCrystalLimit, upperCrystalLimit);
-    diamond_Num = getRandom(lowerCrystalLimit, upperCrystalLimit);
-    purple_Num = getRandom(lowerCrystalLimit, upperCrystalLimit);
-    blue_Num = getRandom(lowerCrystalLimit, upperCrystalLimit);
-    random_Num = randomTarget(targetLowerLimit, targetUpperLimit); 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+function resetGame() {
+    crystals.diamond.value = getRandom(1, 12);
+    crystals.purple.value = getRandom(1, 12)
+    crystals.blue.value = getRandom(1, 12)
+    crystals.red.value = getRandom(1, 12)
+    targetScore = getRandom(19, 120)
+
+    $("#randomNumber").text(targetScore)
+    console.log('crystals here=>', crystals)
+    console.log('DAT target score ===>', targetScore)
+};
+
+function addValue(clickedCrystal) {
+    cuurentScore += clickedCrystal.value
+    $("#randomNumber").text(targetScore)
+}
+
+$("#blue").click(function() {
+    addValue(crystals.blue)
+})
+
+$("#red").click(function() {
+    addValue(crystals.red)
+})
